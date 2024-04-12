bfe526fd47e3731dbe3fd3603af048a269b256e2
Jonathan Parker <jonathanp62@gmail.com>
2024-04-12 17:15:12 -0400

    Completed a thorough refactoring

---
+++ src/main/java/net/jmp/handoff/daemon/Config.java
@@ -0,0 +1,57 @@
+package net.jmp.handoff.daemon;
+
+/*
+ * (#)Config.java   0.4.0   04/12/2024
+ *
+ * @author    Jonathan Parker
+ * @version   0.4.0
+ * @since     0.4.0
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import com.google.gson.annotations.SerializedName;
+
+public class Config {
+    @SerializedName("hostname")
+    private String hostName;
+
+    @SerializedName("port")
+    private int port;
+
+    String getHostName() {
+        return this.hostName;
+    }
+
+    void setHostName(final String hostName) {
+        this.hostName = hostName;
+    }
+
+    int getPort() {
+        return this.port;
+    }
+
+    void setPort(final int port) {
+        this.port = port;
+    }
+}
--- src/main/java/net/jmp/handoff/daemon/Main.java
+++ src/main/java/net/jmp/handoff/daemon/Main.java
@@ -1,12 +1,13 @@
 package net.jmp.handoff.daemon;
 
 /*
+ * (#)Main.java 0.4.0   04/12/2024
  * (#)Main.java 0.3.0   04/12/2024
  * (#)Main.java 0.2.0   04/06/2024
  * (#)Main.java 0.1.0   04/05/2024
  *
  * @author    Jonathan Parker
- * @version   0.3.0
+ * @version   0.4.0
  * @since     0.1.0
  *
  * MIT License
@@ -35,12 +36,26 @@ package net.jmp.handoff.daemon;
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOServer;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import java.util.Optional;
+
 import org.slf4j.LoggerFactory;
 
 import org.slf4j.ext.XLogger;
 
 public final class Main {
+    private static final String APP_CONFIG_FILE = "config/config.json";
+
     private final XLogger logger = new XLogger(LoggerFactory.getLogger(this.getClass().getName()));
+    private final Object stopSerializer = new Object();
+
+    private SocketIOServer server;
 
     /**
      * The constructor.
@@ -55,16 +70,41 @@ public final class Main {
     private void run() {
         this.logger.entry();
 
-        final var serializer = new Object();
+        this.getAppConfig().ifPresent(appConfig -> {
+            this.startServer(appConfig.getHostName(), appConfig.getPort());
+            this.waitAndStopServer();
+        });
 
-        final var config = new Configuration();
+        this.logger.exit();
+    }
 
-        config.setPort(10130);
-        config.setHostname("localhost");
+    private Optional<Config> getAppConfig() {
+        this.logger.entry();
 
-        final var server = new SocketIOServer(config);
+        Config appConfig = null;
 
-        server.addConnectListener(
+        try {
+            appConfig = new Gson().fromJson(Files.readString(Paths.get(APP_CONFIG_FILE)), Config.class);
+        } catch (final IOException ioe) {
+            this.logger.catching(ioe);
+        }
+
+        this.logger.exit(appConfig);
+
+        return Optional.ofNullable(appConfig);
+    }
+
+    private void startServer(final String hostName, final int port) {
+        this.logger.entry(hostName, port);
+
+        final var socketIoConfig = new Configuration();
+
+        socketIoConfig.setPort(port);
+        socketIoConfig.setHostname(hostName);
+
+        this.server = new SocketIOServer(socketIoConfig);
+
+        this.server.addConnectListener(
                 client -> {
                     final var sessionId = client.getSessionId().toString();
 
@@ -73,43 +113,49 @@ public final class Main {
                     client.sendEvent(SocketEvents.CONNECT, "connected");
                 });
 
-        server.addDisconnectListener(
+        this.server.addDisconnectListener(
                 client -> {
                     final var sessionId = client.getSessionId().toString();
 
                     this.logger.info("Client has disconnected: {}", sessionId);
                 });
 
-        server.addEventListener(SocketEvents.VERSION, String.class,
+        this.server.addEventListener(SocketEvents.VERSION, String.class,
                 (client, message, ackRequest) -> {
                     final var sessionId = client.getSessionId().toString();
 
                     this.logger.info("Client sent {} event: {}", SocketEvents.VERSION, sessionId);
-                    client.sendEvent(SocketEvents.VERSION, "Handoff daemon version 0.3.0");
+                    client.sendEvent(SocketEvents.VERSION, "Handoff daemon version " + Version.VERSION);
                 });
 
-        server.addEventListener(SocketEvents.STOP, String.class,
+        this.server.addEventListener(SocketEvents.STOP, String.class,
                 (client, message, ackRequest) -> {
                     final var sessionId = client.getSessionId().toString();
 
                     this.logger.info("Client sent {} event: {}", SocketEvents.STOP, sessionId);
                     client.sendEvent(SocketEvents.STOP, "Handoff daemon stopping...");
 
-                    synchronized (serializer) {
-                        serializer.notifyAll();
+                    synchronized (this.stopSerializer) {
+                        this.stopSerializer.notifyAll();
                     }
                 });
 
-        server.start();
+        this.server.start();
 
-        synchronized (serializer) {
+        this.logger.exit();
+    }
+
+    private void waitAndStopServer() {
+        this.logger.entry();
+
+        synchronized (this.stopSerializer) {
             try {
-                serializer.wait();
+                this.stopSerializer.wait();
             } catch (final InterruptedException ie) {
                 this.logger.catching(ie);
             }
 
-            server.stop();
+            this.server.stop();
         }
 
         this.logger.exit();
